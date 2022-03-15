34048f76d0824878b6968af4de9d0352f1058e81
Navid Tafreshi <navidtrc@gmail.com>
2022-03-15 15:59:39 +0330

    Add Cors Add DB INIT

--- InstituteApi/Startup.cs
+++ InstituteApi/Startup.cs
@@ -37,13 +37,20 @@ namespace InstituteApi
             services.AddHttpClient();
             services.AddMvc();
             services.AddSwaggerGen();
+            services.AddCors();
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.IntializeDatabase();
+
+            app.UseCors(options =>
+                options.WithOrigins("https://localhost:5001", "http://localhost:5000")
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+
+            app.IntializeDatabase();
 
             if (env.IsDevelopment())
             {
